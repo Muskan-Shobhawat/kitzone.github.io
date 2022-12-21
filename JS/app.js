efb5df51a759ef700df825067cf79b376d28582f
Muskan Shobhawat <muskanshibhawat123@gmail.com>
2022-12-21 18:04:41 +0530

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -160,3 +160,50 @@ function myfunction(y) {
         }
     }
 }
+
+var p = window.matchMedia("(max-width: 1080px)");
+myfunction(p);
+function myfunction(p) {
+    if (p.matches) {
+        bb2.addEventListener("click", fun9);
+        let c = 0;
+        let d = 0;
+        function fun9() {
+            bb2.style = "transform:rotate(0deg) translate(-5vw, -45vh); height:60vh; ";
+            bb1.style = "z-index:1; transform:rotate(-20deg) translate(-30vw, 6vh); height:50vh;  ";
+            bbb1.style.height = "50vh";
+            bbb2.style.height = "60vh";
+            c = 1;
+            console.log(c);
+            if (d == 1) {
+                console.log(c + "its query")
+                bb2.style = "z-index:3; transform:rotate(0deg) translate(-5vw, -45vh); height:60vh; ";
+                bb3.style = "z-index:1; transform:rotate(15deg) translate(-29.8vw, -90vh); height:45vh; ";
+                // bb1.style = "z-index:1; transform:rotate(-25deg) translate(-9vw,-12vh); height:45vh;  ";
+                bbb2.style.height = "60vh";
+                bbb3.style.height = "45vh";
+                // bbb1.style.height = "45vh";
+            }
+        }
+
+        bb3.addEventListener("click", fun10);
+        function fun10() {
+            bb3.style = "z-index:3; transform:rotate(0deg) translate(-5vw, -87vh); height:60vh; ";
+            bb2.style = "z-index:2; transform:rotate(-15deg) translate(-2vw,-32vh); height:50vh;  ";
+            bb1.style = "z-index:1; transform:rotate(-25deg) translate(-28vw,12vh); height:45vh; ";
+            bbb1.style.height = "45vh";
+            bbb2.style.height = "50vh";
+            bbb3.style.height = "60vh";
+            d = 1;
+        }
+        bb1.addEventListener("click", fun11);
+        function fun11() {
+            bb3.style = "z-index:1; transform:rotate(25deg) translate(-46vw, -87vh); height:45vh; ";
+            bb2.style = "z-index:2; transform:rotate(15deg) translate(-12vw, -48vh); height:50vh;  ";
+            bb1.style = "z-index:3; transform:rotate(0deg) translate(-5vw, 5vh); height:60vh;  ";
+            bbb1.style.height = "60vh";
+            bbb2.style.height = "50vh";
+            bbb3.style.height = "45vh";
+        }
+    }
+}
